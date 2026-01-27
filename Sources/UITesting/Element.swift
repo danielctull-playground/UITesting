41e5a98d7fe8b3ae05f38f10039081e98b7597ce
Daniel Tull <dt@danieltull.co.uk>
2026-01-27 07:47:55 +0000

    Move Lookup to its own file

--- Sources/UITesting/Element.swift
+++ Sources/UITesting/Element.swift
@@ -12,23 +12,3 @@ extension Element {
     id[keyPath: \.label]
   }
 }
-
-@dynamicMemberLookup
-public struct Lookup<Value> {
-
-  private let action: (Application) -> Value
-
-  init(_ action: @escaping (Application) -> Value) {
-    self.action = action
-  }
-
-  func callAsFunction(_ application: Application) -> Value {
-    action(application)
-  }
-
-  public subscript<New>(
-    dynamicMember keyPath: KeyPath<Value, New>
-  ) -> Lookup<New> {
-    Lookup<New> { action($0)[keyPath: keyPath] }
-  }
-}
