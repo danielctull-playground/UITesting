00fb9d564e9baca46e9ea57d0308845f756abfa8
Daniel Tull <dt@danieltull.co.uk>
2026-05-15 09:26:02 +0100

    Ensure Screen and Element have the same id type
    This allows Never to conform to both Screen and Element.

--- Sources/UITesting/Element.swift
+++ Sources/UITesting/Element.swift
@@ -14,3 +14,14 @@ extension Element {
     Query { id($0)[keyPath: keyPath] }
   }
 }
+
+extension State {
+
+  func element<E: Element>(
+    at keyPath: KeyPath<Content, E>
+  ) throws -> E {
+    let element = content[keyPath: keyPath]
+    try exists(element.id)
+    return element
+  }
+}
