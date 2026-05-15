18447f6e73dac297342b8d846e8edf1c900f1ec4
Daniel Tull <dt@danieltull.co.uk>
2026-05-15 07:30:51 +0100

    Add expect(notExists:) State assertion

--- Sources/UITesting/Asserions/Exists.swift
+++ Sources/UITesting/Asserions/Exists.swift
@@ -20,6 +20,21 @@ extension State {
     try element(at: keyPath)
     return self
   }
+
+  @discardableResult
+  public consuming func expect<E: Element>(
+    notExists keyPath: KeyPath<Content, E>
+  ) throws -> Self {
+    let element = content[keyPath: keyPath]
+    let xcuiElement = element.id(application)
+    let predicate = NSPredicate(format: "exists == false")
+    let expectation = XCTNSPredicateExpectation(predicate: predicate, object: xcuiElement)
+    let result = XCTWaiter().wait(for: [expectation], timeout: 10)
+    guard result == .completed else {
+      throw ElementExists(content: content, element: element)
+    }
+    return self
+  }
 }
 
 // MARK: ElementDoesNotExist
@@ -33,5 +48,19 @@ struct ElementDoesNotExist<Content: Screen, E: Element>: Error {
 extension ElementDoesNotExist: @MainActor CustomStringConvertible {
   var description: String {
     "Element does not exist. \(content) \(element)"
+  }
+}
+
+// MARK: ElementExists
+
+@MainActor
+struct ElementExists<Content: Screen, E: Element>: Error {
+  fileprivate let content: Content
+  fileprivate let element: E
+}
+
+extension ElementExists: @MainActor CustomStringConvertible {
+  var description: String {
+    "Element exists when it was expected to be absent. \(content) \(element)"
   }
 }
