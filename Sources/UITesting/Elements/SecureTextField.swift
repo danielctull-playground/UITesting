134f7457080854bfe5a65479aa497a08e34569d6
Daniel Tull <dt@danieltull.co.uk>
2026-05-15 14:08:23 +0100

    Move type actions

--- Sources/UITesting/Elements/SecureTextField.swift
+++ Sources/UITesting/Elements/SecureTextField.swift
@@ -14,16 +14,3 @@ extension SecureTextField {
     self.init(id: \.secureTextFields[key])
   }
 }
-
-extension State {
-
-  @discardableResult
-  public consuming func type(
-    _ text: String,
-    in keyPath: KeyPath<Content, SecureTextField>
-  ) throws -> Self {
-    let textField = try element(at: keyPath)
-    textField.id(application).typeText(text)
-    return self
-  }
-}
