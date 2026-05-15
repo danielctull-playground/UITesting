fb8347f68be25ddac916fc0181ec34d02cf1661c
Daniel Tull <dt@danieltull.co.uk>
2026-05-15 14:07:25 +0100

    Move tap functions to an Actions folder

--- Sources/UITesting/Element.swift
+++ Sources/UITesting/Element.swift
@@ -26,32 +26,3 @@ extension State {
     return element
   }
 }
-
-// MARK: - Actions
-
-extension State {
-
-  @discardableResult
-  public consuming func tap<E: Element>(
-    _ keyPath: KeyPath<Content, E>
-  ) throws -> Self where E.Destination == Never {
-    let button = try element(at: keyPath)
-    button.id(application).tap()
-    return self
-  }
-
-  @discardableResult
-  public consuming func tap<E: Element>(
-    _ keyPath: KeyPath<Content, E>
-  ) throws -> State<E.Destination> {
-    let button = try element(at: keyPath)
-    button.id(application).tap()
-
-    let destination = State<E.Destination>(
-      application: application,
-      content: E.Destination()
-    )
-
-    return try destination.shows()
-  }
-}
